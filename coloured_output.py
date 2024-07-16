7d62283120c8ddffc55962be412ff803c65f42c4
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-17 00:53:01 +0530

    feat: create coloured_output.py and dummy_data.py

---
+++ coloured_output.py
@@ -0,0 +1,9 @@
+from colorama import Fore
+
+print("Hello World !")
+print(Fore.BLUE+"Hello World !")
+print(Fore.BLACK+"Hello World !")
+print(Fore.RED+"Hello World !")
+print(Fore.WHITE+"Hello World !")
+print(Fore.YELLOW+"Hello World !")
+print(Fore.LIGHTWHITE_EX+"Hello World !")
---
+++ dummy_data.py
@@ -0,0 +1,19 @@
+from faker import Faker
+
+fake = Faker()
+
+print(fake.name())
+
+print(fake.address())
+
+print(fake.email())
+
+print(fake.phone_number())
+
+print(fake.date_of_birth())
+
+print(fake.phone_number())
+
+print(fake.paragraph())
+
+print(fake.sentence())
