e6533061f629d8675bbf5d590fcce58af6642c99
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-18 00:54:08 +0530

    feat: create barcode.py and contdown.py

---
+++ barcode.py
@@ -0,0 +1,14 @@
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+def generate_barcode(data):
+    # Generate Code128 barcode
+    code128 = Code128(data, writer=ImageWriter())
+
+    # Save the barcode image
+    filename = code128.save('barcode')
+
+    print(f"Barcode saved as {filename}")
+
+data = "1234-5678-9012"
+generate_barcode(data)
---
+++ contdown.py
@@ -0,0 +1,32 @@
+import time
+
+seconds = bool(
+    input("do you want to convert minuites into seconds(1 for yes , 0 for no):"))
+if seconds == 1:
+    min = int(input("enter time in minuites:"))
+    sec = min*60
+else:
+    print("you are good to go :)")
+print(f"{min} minuites is {sec} seconds")
+
+
+def count_down(n):
+    while n:
+        mins, sec = divmod(n, 60)
+        timer = '{:02d}:{:02d}'.format(mins, sec)
+        print(timer, end="\r")
+        time.sleep(1)
+        n -= 1
+
+    print('Timer completed!')
+
+
+n = int(input("Enter the time in seconds:"))
+
+count_down(n)
+
+timer = int(input("Enter time in sec:"))
+for i in range(timer, 0, -1):
+    print(i)
+    time.sleep(1)
+print("time out !")
